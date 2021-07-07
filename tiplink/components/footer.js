e07134febfc8c9d0e74eca5864784b35ef90ce95
Aashiq Dheeraj <me@aashiqdheeraj.com>
2021-07-06 22:23:43 -0400

    f

---
+++ tiplink/components/footer.js
@@ -0,0 +1,19 @@
+import styles from '../styles/Home.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      Copyright 2021
+      <a
+        href="https://solana.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className={styles.box}>
+          Powered by{' '}
+          <img src="/solana.png" alt="Solana Logo" width={98} height={40} />
+        </div>
+      </a>
+    </footer>
+  );
+}
