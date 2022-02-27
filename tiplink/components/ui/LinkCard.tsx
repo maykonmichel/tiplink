e3ead814dba04f3482be5b394529cfa3e6a73d42
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-02-27 18:23:36 -0500

    f

---
+++ tiplink/components/ui/LinkCard.tsx
@@ -0,0 +1,30 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import { useLink } from '../useLink';
+
+const LinkCard = () => {
+    const { getBalanceSOL, getBalanceUSD } = useLink();
+    const balanceSOL = getBalanceSOL();
+    const balanceUSD = getBalanceUSD();
+    const url = window.location.href;
+    return(
+        <Card className="linkCard">
+            <CardContent>
+                <Box className="linkCardTop" sx={{display: 'flex', m: 1, justifyContent: 'space-between', alignItems: "center", width: "100%"}} >
+                    <div className="solLogoCard">
+                        <img src="/solanaLogoMark.png" alt="Solana Logo" />
+                    </div>
+                    <Typography>
+                        ${balanceUSD.toFixed(2)}
+                    </Typography>
+                </Box>
+                <Typography>{url}</Typography>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default LinkCard;
