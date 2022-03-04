2ca69140a9d2cb6a0c99debcc6ef9b572fae25d2
Brian Yang <brian@aerodroid.com>
2022-03-03 21:56:32 -0500

    Implemented OptionRow component, rearranged UI.tsx to start mocking out layout, implemented DualCtaRow for send/deposit actions, refactored HeaderLinkExport component and renamed it to LinkExportPanel

---
+++ tiplink/components/ui/LinkExportPanel.tsx
@@ -0,0 +1,54 @@
+import styles from './LinkExportPanel.module.css';
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import {
+  ContentCopy as IconCopy,
+  BookmarkBorder as IconBookmark,
+  QrCodeRounded as IconQrCode,
+} from '@mui/icons-material';
+
+const LinkExportPanel = () => {
+  const [ url, setUrl ] = useState('');
+
+  useEffect(() => {
+      setUrl(window.location.href);
+  }, []);
+
+  return(
+    <Box width='100%'>
+      <Box 
+        className={styles.linkFieldContainer}
+        style={{marginTop: '1rem'}}>
+        <span className={styles.linkFieldText}>{url}</span>
+      </Box>
+
+      <Box sx={{display: 'flex', gap: '1rem', width: '100%', marginTop: '1rem'}}>
+        {renderButton('Copy', <IconCopy/>, () => navigator.clipboard.writeText(url))}
+        {renderButton('Bookmark', <IconBookmark/>, () => {/* TODO */})}
+        {renderButton('QR Code', <IconQrCode/>, () => {/* TODO */})}
+      </Box>
+    </Box>
+  );
+};
+
+function renderButton(label: string, icon: React.ReactNode, onClick: () => void): React.ReactNode {
+  return (
+    <Button
+      style={{
+        display: 'flex', 
+        flexDirection: 'column', 
+        flexGrow: '1', 
+        flexBasis: '0', 
+        padding: '0.75rem 0rem 0.75rem 0rem', 
+        textTransform: 'none'}}
+      variant='outlined'
+      onClick={onClick}>
+      {icon}
+      <Typography variant='subtitle2' marginTop='0.25rem'>{label}</Typography>
+    </Button>
+  );
+}
+
+export default LinkExportPanel;
--- tiplink/components/ui/UI.tsx
+++ tiplink/components/ui/UI.tsx
@@ -1,27 +1,38 @@
-import styles from '../../styles/Home.module.css'
-import Footer  from "../footer";
+import { useLink } from '../useLink';
 
+import styles from '../../styles/Home.module.css'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import Footer  from '../footer';
+
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-const QRCode = require("qrcode.react");
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { useLink } from '../useLink';
-// Default styles that can be overridden by your app
+
 require('@solana/wallet-adapter-react-ui/styles.css');
-import WithdrawForm  from "./WithdrawForm";
-import AddMoneyPhantom from "./AddMoneyPhantom";
-import WithdrawToPhantom from "./WithdrawToPhantom";
-import CreateLinkForm from "./CreateLinkForm";
-import HeaderLinkExport from "./HeaderLinkExport";
-import OptionRow from "./OptionRow";
-import LinkCard from "./LinkCard";
+import {
+  WalletDisconnectButton,
+  WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+const QRCode = require('qrcode.react');
+
+import WithdrawForm  from './WithdrawForm';
+import AddMoneyPhantom from './AddMoneyPhantom';
+import WithdrawToPhantom from './WithdrawToPhantom';
+import CreateLinkForm from './CreateLinkForm';
+
+import LinkExportPanel from './LinkExportPanel';
+import LinkCard from './LinkCard';
+import OptionRow from './OptionRow';
+import DualCtaRow from './DualCtaRow';
+
+import {
+  Refresh as IconRecreate,
+  MergeRounded as IconCombine,
+  AccountBalanceWalletRounded as IconWallet
+} from '@mui/icons-material';
 
 const theme = createTheme({
   palette: {
@@ -41,67 +52,88 @@ const theme = createTheme({
 });
 
 const UI = () => {
-  // const explorerLink = "https://explorer.solana.com/address/" + linkKeypair.publicKey.toString() + "?cluster=" + endpoint;
+  // const explorerLink = 'https://explorer.solana.com/address/' + linkKeypair.publicKey.toString() + '?cluster=' + endpoint;
   const { linkKeypair, getBalanceUSD } = useLink();
   const balanceUSD = getBalanceUSD();
   return(
     <ThemeProvider theme={theme}>
-      <AppBar position="sticky" className="appbar" style={{ background: '#ffffff' }} elevation={0}>
+      <AppBar position='sticky' className='appbar' elevation={0}>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
-
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}></Typography>
           <WalletMultiButton/>
           <WalletDisconnectButton/>
         </Toolbar>
       </AppBar>
+      
       <div className={styles.container}>
         <main className={styles.main}>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: '100vh' }}
-        >
-          <Typography component="div" variant="h5">This is ${balanceUSD.toFixed(2)} in crypto.</Typography>
-          <Typography component="div">The link to this page contains this value, make sure you don't lose it!</Typography>
+          <Grid
+            container
+            maxWidth='32rem'
+            direction='column'
+            alignItems='center'>
+              <Box 
+                width='100%'
+                marginTop='4rem'
+                textAlign='center'>
+                <Typography component='div' variant='h5'>This is ${balanceUSD.toFixed(2)} in crypto.</Typography>
+                <Typography component='div'>The link to this page contains this value, make sure you don't lose it!</Typography>
+                <LinkExportPanel/>
+              </Box>
 
-          <HeaderLinkExport/>
+              <Box
+                width='100%'
+                marginTop='2rem'
+                marginBottom='1.5rem'>
+                <LinkCard/>
+              </Box>
 
-          {/* <LinkCard/> */}
-          <Typography component="div">Public Key: {linkKeypair.publicKey.toBase58()}</Typography>
-          <WithdrawForm/>
-          <br></br>
-          <AddMoneyPhantom/>
-          <br></br>
-          <WithdrawToPhantom/>
-          <CreateLinkForm/>
-          <br></br>
+              <Box width='100%'>
+                <DualCtaRow
+                  cta1Label='Send'
+                  cta2Label='Deposit'
+                  cta1OnClick={() => {
 
-          <div style={{maxWidth: '36rem'}}>
-            <Divider/>
-            <OptionRow
-              title='Use QR code'
-              subtitle='Show a QR code for this wallet. You can scan it with any Solana-compatible wallet.'
-            />
-            <Divider/>
-          </div>
+                  }}
+                  cta2OnClick={() => {
 
-          {/* {endpoint === "devnet" && 
-            <AirdropForm />
-          } */}
-  
-        </Grid>
+                  }}/>
+                <Divider/>
+                <OptionRow
+                  icon={<IconRecreate/>}
+                  title='Recreate this TipLink'
+                  subtitle='Move the entire value to a new TipLink so only you have the link.'/>
+                <Divider/>
+                <OptionRow
+                  icon={<IconCombine/>}
+                  title='Combine with another TipLink'
+                  subtitle='You can combine some or all of another TipLink\’s value into this TipLink.'/>
+                <Divider/>
+                <OptionRow
+                  icon={<IconWallet/>}
+                  title='Deposit from your wallet'
+                  subtitle='Deposit Solana from your connected wallet.'/>
+                <Divider/>
+              </Box>
 
+              {/* {endpoint === 'devnet' && 
+                <AirdropForm />
+              } */}
 
+              <Typography marginTop='8rem' component='div' textAlign='center'>[CONTENT BELOW THIS IS TEMPORARY]<br/>Public Key: {linkKeypair.publicKey.toBase58()}</Typography>
+              <WithdrawForm/>
+              <br></br>
+              <AddMoneyPhantom/>
+              <br></br>
+              <WithdrawToPhantom/>
+              <CreateLinkForm/>
+              <br></br>
+          </Grid>
         </main>
         <Footer/>
       </div>
     </ThemeProvider>
-
   );
-
 }
 
 export default UI;
