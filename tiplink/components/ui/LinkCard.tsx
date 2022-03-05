46cb905ae040940478bd3901d8cc480225128f9d
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-05 11:30:14 -0500

    LinkCard looks kind of ok now

--- tiplink/components/ui/LinkCard.tsx
+++ tiplink/components/ui/LinkCard.tsx
@@ -6,20 +6,29 @@ import Avatar from '@mui/material/Avatar';
 import { useLink } from '../useLink';
 
 const LinkCard = () => {
-    const { balanceSOL, balanceUSD } = useLink();
-    const url = window.location.href;
+    const { linkKeypair, balanceSOL, balanceUSD } = useLink();
+    const getCardDisplayText = () => {
+        const url = window.location.href;
+        if(url.search("wallet") != -1) {
+            return linkKeypair.publicKey.toBase58();
+        } 
+        return window.location.pathname.substr(1) + window.location.hash;
+    }
+
+    const cdt = getCardDisplayText();
     return(
-        <Card className="linkCard">
-            <CardContent>
+        <Card className="linkCard" style={{width: "32rem", height: "20rem", borderRadius: "2rem", justifyContent: "center"}} raised={true}>
+            <CardContent sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", textAlign: "center"}}>
                 <Box className="linkCardTop" sx={{display: 'flex', m: 1, justifyContent: 'space-between', alignItems: "center", width: "100%"}} >
-                    <div className="solLogoCard">
-                        <img src="/solanaLogoMark.png" alt="Solana Logo" />
+                    <div style={{display: "flex", borderRadius: "100%", backgroundColor: "black", textAlign: "center", width: "5rem", height: "5rem", justifyContent: "center", alignItems: "center"}}>
+                        <img src="/solanaLogoMark.png" alt="Solana Logo" width="60%" height="60%" style={{top: 0, left: 0}}/>
                     </div>
-                    <Typography>
-                        ${balanceUSD.toFixed(2)}
-                    </Typography>
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        <Typography variant="h4"> ${balanceUSD.toFixed(2)} </Typography>
+                        <Typography > {balanceSOL.toFixed(4)} SOL</Typography>
+                    </div>
                 </Box>
-                <Typography>{url}</Typography>
+                <Typography style={{fontFamily: "Courier", fontWeight: "bold"}}>{cdt}</Typography>
             </CardContent>
         </Card>
     );
