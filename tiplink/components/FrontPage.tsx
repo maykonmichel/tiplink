6e25e98e6dff7d1ed58f9acd195446b709c54780
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-14 14:53:37 -0400

    take 2 of fixing the meta

--- tiplink/components/FrontPage.tsx
+++ tiplink/components/FrontPage.tsx
@@ -78,7 +78,9 @@ export default function FrontPage() {
         <meta name="description" content="Send tip links with crypto" />
         <meta property="og:title" content="You received some crypto!" />
         <meta property="og:url" content="https://www.tiplink.io" />
-        <meta property="og:image" content="https://tiplink.io/tiplink-card-preview.png" />
+        <meta property="og:image" content="http://tiplink.io/tiplink-card-preview.png" />
+        <meta property="og:image:secure_url" content="https://tiplink.io/tiplink-card-preview.png" />
+        <meta property="og:image:type" content="image/png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <WalletAppBar/>
