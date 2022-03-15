c796e2980c132bbb9bc737ae2b4080b1bbd7fd2f
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-15 18:59:55 -0400

    various security related headers

--- tiplink/next.config.js
+++ tiplink/next.config.js
@@ -1,3 +1,28 @@
+// next.config.js
+
+// https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src 'self';
+  style-src 'self';
+  font-src 'self';  
+`
+const securityHeaders = [
+  // just for perf
+  {key: 'X-DNS-Prefetch-Control', value: 'on'},
+  // specified by default on next.js, only need to override because we're overriding headers
+  {key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload'},
+  // csp should make this irrelevant, but just in case
+  {key: 'X-XSS-Protection', value: '1; mode=block'},
+  // frustrate xss attacks that change content type
+  {key: 'X-Content-Type-Options', value: 'nosniff'},
+  // strip referrer always
+  {key: 'Referrer-Policy', value: 'no-referrer'},
+  // the bulk of xss protection is this guy
+  {key: 'Content-Security-Policy', value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()}
+]
+
 module.exports = {
   future: {
     webpack5: true, // by default, if you customize webpack config, they switch back to version 4. 
@@ -11,5 +36,14 @@ module.exports = {
     };
 
     return config;
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 };
