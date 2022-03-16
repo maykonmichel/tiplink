7e61057a01ab7863689cd50dd96ab7f1306b1496
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-16 16:26:22 -0400

    forgot to add Footer

---
+++ tiplink/components/ui/common/Footer.tsx
@@ -0,0 +1,22 @@
+import styles from '../../../styles/Home.module.css';
+
+export default function Footer() {
+  return (
+    <div>
+      <div className={styles.footerPhantom}/>
+      <footer className={styles.footer}>
+        Copyright 2022
+        <a
+          href="https://solana.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className={styles.box}>
+            Powered by{' '}
+            <img src="/solana.png" alt="Solana Logo" width={98} height={40} />
+          </div>
+        </a>
+      </footer>
+    </div>
+  );
+}
