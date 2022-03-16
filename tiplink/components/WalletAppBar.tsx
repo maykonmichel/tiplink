bbf77d6b7694bcaff2936c1b482ae40908ae1450
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-16 16:24:45 -0400

    fix logo and footer a bit for mobile

--- tiplink/components/WalletAppBar.tsx
+++ tiplink/components/WalletAppBar.tsx
@@ -1,6 +1,7 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
+import styles from '../styles/Home.module.css';
 import Typography from '@mui/material/Typography';
 require('@solana/wallet-adapter-react-ui/styles.css');
 import {
@@ -13,8 +14,8 @@ const WalletAppBar = () => {
       <AppBar color='transparent' position='relative' className='appbar' elevation={0} sx={{padding: '1rem'}}>
         <Toolbar>
           <Box component='div' sx={{ display: "flex", flexGrow: 1, flexDirection: "row" }}> 
-            <img src='/tiplink-logo.png' width='200px'/>
-            <Typography variant="h6" style={{marginLeft: '0.5rem'}}>BETA</Typography>
+            <img className={styles.tiplinkLogo} src='/tiplink-logo.png' width='200px'/>
+            <Typography className={styles.tiplinkBeta}>BETA</Typography>
           </Box>
           <Box sx={{display: 'flex', gap: '1rem'}}>
             <WalletMultiButton/>
--- tiplink/pages/index.tsx
+++ tiplink/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import FrontPage from "../components/FrontPage";
 import SLWallet from "../components/SLWallet";
 import Progress from "../components/ui/common/Progress";
-import Footer from "../components/ui/Footer";
+import Footer from "../components/ui/common/Footer";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
