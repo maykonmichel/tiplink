1f86b0c7b15cf7c0e409faa1dee255a9d05703b0
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-16 19:52:33 -0400

    some layout refactor, get footer to work again

--- tiplink/components/ui/common/Footer.tsx
+++ tiplink/components/ui/common/Footer.tsx
@@ -2,21 +2,21 @@ import styles from '../../../styles/Home.module.css';
 
 export default function Footer() {
   return (
-    <div>
-      <div className={styles.footerPhantom}/>
-      <footer className={styles.footer}>
-        Copyright 2022
-        <a
-          href="https://solana.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className={styles.box}>
-            Powered by{' '}
-            <img src="/solana.png" alt="Solana Logo" width={98} height={40} />
-          </div>
-        </a>
-      </footer>
+    <div className={styles.footer}>
+      Copyright 2022
+      <a
+        href="https://solana.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className={styles.box}>
+          Powered by{' '}
+          <img src="/solana.png" alt="Solana Logo" width={98} height={40} />
+        </div>
+      </a>
+      <div>
+        <a href="/faq">FAQ</a>
+      </div>
     </div>
   );
 }
