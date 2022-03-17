aab33ad1730f53cb9a684691d8a66c2f11d5031c
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-03-17 14:54:57 -0400

    twitter and discord links

--- tiplink/components/ui/common/Footer.tsx
+++ tiplink/components/ui/common/Footer.tsx
@@ -1,4 +1,6 @@
 import styles from '../../../styles/Home.module.css';
+import { Twitter } from '@mui/icons-material';
+import 'material-icons/iconfont/material-icons.css';
 
 export default function Footer() {
   return (
@@ -14,8 +16,18 @@ export default function Footer() {
           <img src="/solana.png" alt="Solana Logo" width={98} height={40} />
         </div>
       </a>
-      <div>
+      <div className={styles.footerLinks}>
         <a href="/faq">FAQ</a>
+
+        <div className={styles.footerSocial}>
+          <a href="https://twitter.com/TipLinkOfficial" 
+          target="_blank" rel="noopener noreferrer">
+            <Twitter />
+          </a>
+          <a href="#" onClick={(e) => {e.preventDefault(); return false;}}>
+            <span className='material-icons'>discord</span>
+          </a>
+        </div>
       </div>
     </div>
   );
