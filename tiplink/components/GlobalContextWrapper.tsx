001855f30113f879c26fdcf475acd12a4acf66dd
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-04-03 18:55:48 -0400

    glow print stmts

--- tiplink/components/GlobalContextWrapper.tsx
+++ tiplink/components/GlobalContextWrapper.tsx
@@ -16,16 +16,55 @@ import {
 
 import React, { FC, ReactNode } from "react";
 import { ExchangeRateProvider } from "./useExchangeRate";
+import { useEffect } from "react";
+import { Transaction, SendOptions, TransactionSignature } from '@solana/web3.js';
+import {
+    EventEmitter,
+} from '@solana/wallet-adapter-base';
 
 export interface GlobalContextWrapperProps {
     children: ReactNode;
 };
 
+interface GlowWalletEvents {
+    connect(...args: unknown[]): unknown;
+    disconnect(...args: unknown[]): unknown;
+}
+
+interface GlowWallet extends EventEmitter<GlowWalletEvents> {
+    isGlow?: boolean;
+    publicKey?: { toBytes(): Uint8Array };
+    isConnected: boolean;
+    signTransaction(transaction: Transaction): Promise<Transaction>;
+    signAllTransactions(transactions: Transaction[]): Promise<Transaction[]>;
+    signAndSendTransaction(
+        transaction: Transaction,
+        options?: SendOptions
+    ): Promise<{ signature: TransactionSignature }>;
+    signMessage(message: Uint8Array): Promise<{ signature: Uint8Array }>;
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+}
+
+interface GlowWindow extends Window {
+    glowSolana?: GlowWallet;
+}
+declare const window: GlowWindow;
+
 const WithEndpoint  : FC<GlobalContextWrapperProps> = ({ children }) => {
     const { endpointUrl } = useEndpoint();
     const wallets = useMemo(
-        () => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new GlowWalletAdapter()], [endpointUrl]
+        () => [
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+            new GlowWalletAdapter()
+        ], [endpointUrl]
     );
+
+    useEffect(() => {
+        console.log("glowSolana", window.glowSolana);
+        console.log("glowSolana.isGlow", window.glowSolana?.isGlow);
+    }, []);
     return (
         <ConnectionProvider endpoint={endpointUrl}>
             <WalletProvider wallets={wallets} autoConnect>
