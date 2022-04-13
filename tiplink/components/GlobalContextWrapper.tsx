9c0261e9e0a14e3c2f134025b3b79fd2813ee18d
Aashiq Dheeraj <me@aashiqdheeraj.com>
2022-04-13 08:41:08 -0400

    update wallet adapter package, add Slope wallet to supported wallets

--- tiplink/components/GlobalContextWrapper.tsx
+++ tiplink/components/GlobalContextWrapper.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from '@mui/material';
 
 import { EndpointProvider } from '../components/EndpointProvider';
 import { useEndpoint } from '../components/useEndpoint';
-
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-import { SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {GlowWalletAdapter } from '@solana/wallet-adapter-glow'
+import { 
+    SolflareWalletAdapter,
+    PhantomWalletAdapter, 
+    GlowWalletAdapter,
+    SlopeWalletAdapter
+} from '@solana/wallet-adapter-wallets';
 import { useMemo } from "react";
 const QRCode = require("qrcode.react");
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
@@ -29,7 +31,8 @@ const WithEndpoint  : FC<GlobalContextWrapperProps> = ({ children }) => {
         () => [
             new PhantomWalletAdapter(),
             new SolflareWalletAdapter(),
-            new GlowWalletAdapter()
+            new GlowWalletAdapter(),
+            new SlopeWalletAdapter()
         ], [endpointUrl]
     );
 
